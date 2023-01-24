fa5a2a63d8d864eb8f7e7c1362213d572e6463e5
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2023-01-24 00:28:28 -0800

    Update index.js
    Completed questions/functions within index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -4,14 +4,97 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    ''
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of project?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Insert project title.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project.',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert description of project..' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write installation information for your project.',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert installation information.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write usage instrutions for your project.',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert project usage instructions.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Write instructions on how to contribute to this project.',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert contribution instructions' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Write test instructions for project.',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert test instructions.' }
+        },
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ["GPLv3", "MIT", "Apache"],
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Choose a license.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Github username?',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert Github username.' }
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email address?',
+        validate: ( value ) => {
+            if ( value ) { return true } else { return 'Insert your email.' }
+        },
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('README generated.');
+            }
+        })
+    };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+    })
+};
 
 // Function call to initialize app
 init();
